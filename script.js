7722bc7e0943a3b1e29d26045e9ba7bdc3bc1b5f
Anuj <anujkrgupta21@gmail.com>
2023-09-03 22:14:23 +0530

    initialcommit

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const fetchDataButton = document.getElementById("fetch-data");
-    const mapContainer = document.getElementById("map-container");
-    const latitudeElement = document.getElementById("latitude");
-    const longitudeElement = document.getElementById("longitude");
-    const temperatureElement = document.getElementById("temperature");
-    const humidityElement = document.getElementById("humidity");
-    const windSpeedElement = document.getElementById("wind-speed");
+// Constants for API URLs
+const GEOLOCATION_API_URL = "https://api.openweathermap.org/data/2.5/onecall";
+const OPENWEATHERMAP_API_KEY = "db1b373fae215624f8fc06fe8dd697fd"; // Replace with your OpenWeatherMap API key
 
-    fetchDataButton.addEventListener("click", () => {
-        // Fetch Geolocation
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(async (position) => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
+// Function to fetch geolocation
+function getGeolocation() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
 
-                // Display Map
-                displayMap(latitude, longitude);
+            // Call functions to display map and fetch weather data
+            displayMap(latitude, longitude);
+            fetchWeatherData(latitude, longitude);
+        });
+    } else {
+        alert("Geolocation is not supported in this browser.");
+    }
+}
 
-                // Fetch Weather Data from OpenWeatherMap API
-                const apiKey = "YOUR_OPENWEATHERMAP_API_KEY";
-                const weatherData = await fetchWeatherData(apiKey, latitude, longitude);
+// Function to display Google Map
+function displayMap(latitude, longitude) {
+    const mapDiv = document.getElementById("map");
 
-                // Display Weather Data
-                displayWeatherData(weatherData);
-
-                // Show map and weather data container
-                mapContainer.style.display = "block";
-            }, (error) => {
-                console.error("Error getting geolocation:", error);
-            });
-        } else {
-            console.error("Geolocation is not supported in this browser.");
-        }
+    // Use the latitude and longitude to create a Google Map
+    const map = new google.maps.Map(mapDiv, {
+        center: { lat: latitude, lng: longitude },
+        zoom: 10,
     });
+}
 
-    // Function to display the user's location on Google Maps
-    function displayMap(latitude, longitude) {
-        // Implement Google Maps integration here
-        // You'll need to include the Google Maps JavaScript API
-    }
+// Function to fetch weather data from OpenWeatherMap
+function fetchWeatherData(latitude, longitude) {
+    const apiKey = OPENWEATHERMAP_API_KEY;
+    
+    // Make a GET request to the OpenWeatherMap API
+    fetch(`${GEOLOCATION_API_URL}?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Process and display weather data in the weatherData div
+            const weatherDataDiv = document.getElementById("weatherData");
+            // Example: weatherDataDiv.innerHTML = `Temperature: ${data.current.temp}°C`;
 
-    // Function to fetch weather data from OpenWeatherMap API
-    async function fetchWeatherData(apiKey, latitude, longitude) {
-        const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-        const response = await fetch(apiUrl);
-        return await response.json();
-    }
+            // Handle other weather data as needed
+        })
+        .catch((error) => {
+            console.error("Error fetching weather data:", error);
+        });
+}
 
-    // Function to display weather data on the page
-    function displayWeatherData(weatherData) {
-        // Extract and display relevant weather details from the API response
-        latitudeElement.textContent = weatherData.lat;
-        longitudeElement.textContent = weatherData.lon;
-        temperatureElement.textContent = weatherData.current.temp;
-        humidityElement.textContent = weatherData.current.humidity;
-        windSpeedElement.textContent = weatherData.current.wind_speed;
-    }
-});
+// Attach the getGeolocation function to the button click event
+document.getElementById("fetchDataBtn").addEventListener("click", getGeolocation);
